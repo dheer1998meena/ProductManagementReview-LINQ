9af3b290dffd920a43d0fe7c8246c2451fc1077a
dheer meena <dheer1998meena@gmail.com>
2020-11-13 16:14:29 +0530

    [Dheer] Add: Implemented the functionality to retrieve the records from data table with like true

--- ProductReviewManagement-LINQ/ProductReviewDataTable.cs
+++ ProductReviewManagement-LINQ/ProductReviewDataTable.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyrightProductReviewDataTable.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Dheer Singh Meena"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Text;
@@ -9,7 +15,7 @@ namespace ProductReviewManagement_LINQ
     {
        public static DataTable table = new DataTable();
         /// <summary>
-        /// UC8: Add data into data table.
+        /// UC8 Add data into data table.
         /// </summary>
         public static void AddDataIntoDataTable()
         {
@@ -22,16 +28,32 @@ namespace ProductReviewManagement_LINQ
             table.Columns.Add("IsLike", typeof(bool));
             ///Adding rows in DataTable
             table.Rows.Add(101, 1, 1, "Low", true);
-            table.Rows.Add(102, 2, 1, "Low", true);
+            table.Rows.Add(102, 2, 1, "Low", false);
             table.Rows.Add(103, 3, 4, "Good", true);
-            table.Rows.Add(104, 4, 5, "Nice", true);
+            table.Rows.Add(104, 4, 5, "Nice",false);
             table.Rows.Add(105, 5, 4, "Good", true);
-            table.Rows.Add(106, 6, 3, "Average", true);
+            table.Rows.Add(106, 6, 3, "Average", false);
             table.Rows.Add(104, 7, 5, "Nice", true);
             table.Rows.Add(105, 8, 3, "Average", true);
             //Printing data
             Console.WriteLine("\nDataTable contents:");
             foreach (var list in table.AsEnumerable())
+            {
+                Console.WriteLine("Product Id :" + list.Field<int>("ProductId") + "\t" + "User Id :" + list.Field<int>("UserId") + "\t" + "Rating ;" + list.Field<double>("Rating") + "\t" + "Review :" + list.Field<string>("Review") + "\t" + "Is Like :" + list.Field<bool>("IsLike"));
+            }
+           
+        }
+        /// <summary>
+        ///  UC9 Retrieves all records whose is like is true.
+        /// </summary>
+        public static void RetrieveRecordWithTrueIsLike()
+        {
+            var retrieveData = from records in table.AsEnumerable()
+                                where (records.Field<bool>("IsLike")==true)
+                                   select records;
+            //Printing data
+            Console.WriteLine("\nRecords in table whose IsLike value is true:");
+            foreach (var list in retrieveData)
             {
                 Console.WriteLine("Product Id :" + list.Field<int>("ProductId") + "\t" + "User Id :" + list.Field<int>("UserId") + "\t" + "Rating ;" + list.Field<double>("Rating") + "\t" + "Review :" + list.Field<string>("Review") + "\t" + "Is Like :" + list.Field<bool>("IsLike"));
             }
